f56ec4b0b88a1b518d7b2410fbfedf5f76595d32
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-08-14 23:04:14 -0300

    Deleted ignore folder

--- _ignore/game_test/game_test.py
+++
@@ -1,4 +0,0 @@
-from pygin import *
-from .scripts.game_settings import GameSettings
-
-Engine.start_game(GameSettings)
--- _ignore/game_test/scripts/game_settings.py
+++
@@ -1,9 +0,0 @@
-from _ignore.game_test.scripts.scene_controller_script import ScenesControllerScript
-
-
-class GameSettings:
-
-    game_name = "TestGame"
-    screen_width = 450
-    screen_height = 450
-    scenes_list = ScenesControllerScript.get_scenes()
